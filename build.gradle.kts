056aa9e171ca74cf088b4d9ca13f8cd6066f95ba
cheroliv <cheroliv@users.noreply.github.com>
2026-07-06 19:40:38 +0200

    chore(cnv): bump education.cccp.build.* 0.0.1 -> 0.0.2 (harmonisation conventions-gradle)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("education.cccp.build.cucumber") version "0.0.1"
+    id("education.cccp.build.cucumber") version "0.0.2"
     id("education.cccp.container-provision") version "0.0.1"
 }
 
