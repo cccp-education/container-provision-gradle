1125d8df67bf85c43d3484535ad629161cfdb8e4
cheroliv <cheroliv@users.noreply.github.com>
2026-08-06 13:08:01 +0200

    chore: sync pending changes (wrapper/libs/gov) post S-095 + S-071

---
+++ colab-runner/build.gradle.kts
@@ -0,0 +1,1 @@
+plugins {  id("education.cccp.container-provision").version ("0.0.1")}
--- container-provision-plugin/build.gradle.kts
+++ container-provision-plugin/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     `java-library`
     `java-gradle-plugin`
     `maven-publish`
-    kotlin("jvm") version "2.3.20"
+    kotlin("jvm") version "2.4.10"
 }
 
 group = "education.cccp"
